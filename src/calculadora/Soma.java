7c0d1f0943c2127c4021ea8d74ca1bd864ae22cd
pedrojesus44 <pedro.jesus44@etec.sp.gov.br>
2022-05-17 08:17:56 -0300

    v2

--- src/calculadora/Soma.java
+++ src/calculadora/Soma.java
@@ -21,6 +21,8 @@ public class Soma extends JFrame{
     JButton divisao;
     JButton subtracao;
     JButton limpar;
+    JButton ocultar, exibir2;
+    JButton desabilitar, habilitar;
     public Soma() {
         super("Exemplo de soma");
         Container tela = getContentPane();
@@ -35,15 +37,19 @@ public class Soma extends JFrame{
         divisao = new JButton("Divisão");
         subtracao = new JButton("Subtração");
         limpar = new JButton("Limpar");
+        ocultar = new JButton("Ocultar");
+        exibir2 = new JButton("Exibir");
+        desabilitar = new JButton("Desabilitar");
+        habilitar = new JButton("Habilitar");
         
         rotulo1.setBounds(50,20,100,20); rotulo2.setBounds(50,60,100,20);
         texto1.setBounds(120,20,200,20); texto2.setBounds(120,60,200,20);
         exibir.setBounds(50,120,200,20); somar.setBounds(50,100,80,20);
         multiplicacao.setBounds(130, 100, 80, 20); divisao.setBounds(210, 100, 80, 20);
-        subtracao.setBounds(290, 100, 80, 20);
-        limpar.setBounds(170, 150, 80, 20);
-        
-        
+        subtracao.setBounds(290, 100, 80, 20); limpar.setBounds(170, 130, 80, 20);
+        ocultar.setBounds(50, 160, 80, 20); exibir2.setBounds(130, 160, 80, 20);
+        desabilitar.setBounds(210, 160, 80, 20); habilitar.setBounds(290, 160, 80, 20);
+             
         somar.addActionListener(
                 new ActionListener(){
                     public void actionPerformed(ActionEvent e){
@@ -107,15 +113,43 @@ public class Soma extends JFrame{
                     }
                 }
             );
+        ocultar.addActionListener(
+            new ActionListener() {
+               public void actionPerformed(ActionEvent e) {
+                   rotulo1.setVisible(false);
+                   rotulo2.setVisible(false);
+               } 
+            });
+        exibir2.addActionListener(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    rotulo1.setVisible(true);
+                    rotulo2.setVisible(true);
+                }
+            });
+        desabilitar.addActionListener(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    rotulo1.setEnabled(false);
+                    rotulo2.setEnabled(false);
+                }
+            });
+        habilitar.addActionListener(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    rotulo1.setEnabled(true);
+                    rotulo2.setEnabled(true);
+                }
+            });
         exibir.setVisible(false);
         
         tela.add(rotulo1); tela.add(rotulo2);
         tela.add(texto1); tela.add(texto2);
         tela.add(exibir); tela.add(somar);
-        tela.add(multiplicacao);
-        tela.add(divisao);
-        tela.add(subtracao);
-        tela.add(limpar);
+        tela.add(multiplicacao); tela.add(divisao);
+        tela.add(subtracao); tela.add(limpar);
+        tela.add(ocultar); tela.add(exibir2);
+        tela.add(desabilitar); tela.add(habilitar);
         
         setSize(400, 250);
         setVisible(true);
